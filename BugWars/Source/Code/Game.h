02e0900f0596efcd1d4847c345d6dd9d9f553adc
donnola <gabbasova.aa@phystech.edu>
2022-11-08 13:47:40 +0300

    Update Game.h

--- BugWars/Source/Code/Game.h
+++ BugWars/Source/Code/Game.h
@@ -26,8 +26,8 @@ struct Game : public GameBase
 	GameObject* tank_obj;
 	float render_rad_2 = 1000 * 1000;
 	std::vector<GameObject*> objects;
-	const float cell_size = 600;
+	const float cell_size = 800;
 	const float field_size = 9600;
-	const int cells_dim = 16;
+	const int cells_dim = 12;
 	std::vector<std::vector<std::set<Bug*>>> obj_grid;
 };
